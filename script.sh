846ba657b8047076018e35d904b98f4b3ee16c26
Atharva <156517434+athcodes2005@users.noreply.github.com>
2024-10-11 16:10:00 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 # Function to check the internet connection
 check_internet() {
-    # Checks internet connection to google using wget with a single try and 10-second timeout
-    wget -q --spider --tries=1 --timeout=10 https://www.google.com
+    # Checks internet connection to google using wget with a single try and 3-second timeout
+    wget -q --spider --tries=1 --timeout=3 https://www.google.com
     # Returns the exit status of wget command
     return $?
 }
@@ -13,7 +13,7 @@ if ! check_internet; then
     echo "No internet connection. Attempting to authenticate..."
 
     #Authenticates using LDAP credentials 
-    curl --interface eth0 --location-trusted -u ldapid:internettoken "https://internet.iitb.ac.in/login.php" >/dev/null
+    curl --location-trusted -u (ldapid):(internetaccesstoken) "https://internet.iitb.ac.in/login.php" >/dev/null
 else
     echo "Internet connection is active."
 fi
